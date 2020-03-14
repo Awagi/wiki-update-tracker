10c478da41789662782ba843907609f77dc879e1
Awagi <>
2020-03-14 19:12:19 +0100

    Setup exit code 1 for action

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,10 @@
 # 7: bot label
 # 8: token
 # 9: log level (DEBUG, INFO, WARNING or CRITICAL)
-python /usr/src/app/wiki-update.py $1 $2 $3 $4 $5 $6 $7 $8 $9
+if python /usr/src/app/wiki-update.py $1 $2 $3 $4 $5 $6 $7 $8 $9; then
+    echo "Script failed"
+    exit 1
+fi
 
 echo "::set-output name=translation-status::${translation-status}"
 echo "::set-output name=open-issues::${open-issues}"
