5f680950979d90a94096eafba6f9680b8429d396
Awagi <>
2020-07-11 19:05:13 +0200

    :bug: Fixed entrypoint?

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,21 +46,21 @@ arg_projectcardorphantemplate=${36}  # 36) project-card-orphan-template
 [ -n "$arg_loglevel" ] && args="$args -l $arg_loglevel"
 [ -n "$arg_repopath" ] && args="$args -r $arg_repopath"
 if [ -n "$arg_filters" ]; then
-    IFS='\n' read -ra array_filters <<< "$arg_filters"
+    IFS="\n" read -ra array_filters <<< "$arg_filters"
     for x in ${array_filters[@]}; do
         filters="$filters $x"
     done
-    args="$args --filters$filters"
+    args="$args --filter$filters"
 fi
 if [ -n "$arg_ignores" ]; then
-    IFS='\n' read -ra array_ignores <<< "$arg_ignores"
+    IFS="\n" read -ra array_ignores <<< "$arg_ignores"
     for x in ${array_ignores[@]}; do
         ignores="$ignores $x"
     done
     args="$args -i$ignores"
 fi
 if [ -n "$arg_genstubs" ]; then
-    IFS='\n' read -ra array_genstubs <<< "$arg_genstubs"
+    IFS="\n" read -ra array_genstubs <<< "$arg_genstubs"
     for x in ${array_genstubs[@]}; do
         genstubs="$genstubs $x"
     done
@@ -69,7 +69,7 @@ fi
 [ -n "$arg_stubcommit" ] && args="$args --stub-commit \"$arg_stubcommit\""
 [ -n "$arg_stubtemplate" ] && args="$args --stub-template \"$arg_stubtemplate\""
 if [ -n "$arg_gencopy" ]; then
-    IFS='\n' read -ra array_gencopy <<< "$arg_gencopy"
+    IFS="\n" read -ra array_gencopy <<< "$arg_gencopy"
     for x in ${array_gencopy[@]}; do
         gencopy="$gencopy $x"
     done
@@ -82,7 +82,7 @@ if [ "$arg_requestmerge" == "true" ] || [ "$arg_requestmerge" == "1" ]; then
     args="$args --request-merge"  # request-merge
 fi
 if [ -n "$arg_instructissues" ]; then
-    IFS='\n' read -ra array_instructissues <<< "$arg_instructissues"
+    IFS="\n" read -ra array_instructissues <<< "$arg_instructissues"
     for x in ${array_instructissues[@]}; do
         instructissues="$instructissues $x"
     done
@@ -96,7 +96,7 @@ fi
 [ -n "$arg_issueuptodatetemplate" ] && args="$args --issue-uptodate-template $arg_issueuptodatetemplate"
 [ -n "$arg_issueorphantemplate" ] && args="$args --issue-orphan-template $arg_issueorphantemplate"
 if [ -n "$arg_instructprojects" ]; then
-    IFS='\n' read -ra array_instructprojects <<< "$arg_instructprojects"
+    IFS="\n" read -ra array_instructprojects <<< "$arg_instructprojects"
     for x in ${array_instructprojects[@]}; do
         instructprojects="$instructprojects $x"
     done
@@ -117,7 +117,7 @@ fi
 
 # set positional arguments
 args="$args $arg_original"
-IFS='\n' read -ra array_translations <<< "${arg_translations}"
+IFS="\n" read -ra array_translations <<< "${arg_translations}"
 for x in ${array_translations[@]}; do
     translations="$translations $x"
 done
